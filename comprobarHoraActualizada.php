7a8a55d623813580b94f0cb6bb1e2469cd7f35e9
andogoiko <72077752+andogoiko@users.noreply.github.com>
2022-03-31 12:50:15 +0200

    ªª

---
+++ comprobarHoraActualizada.php
@@ -0,0 +1,21 @@
+// se comprueba la hora de la última actualización y se actualiza
+
+date_default_timezone_set('europe/madrid'); //obligamos a la hora a ser como la zona GMT de Madrid
+
+$sql = "SELECT * FROM social_media";
+$result = mysql_query($sql);
+
+$add = round(60 * 60);
+$lastRefreshDate = strtotime(mysql_fetch_array($result)["fechaMod"]) + $add;
+
+$dateNow = strtotime(date("Y/m/d H:i:s")) + $add;
+
+$delay = 3600 * 2.5;
+
+$timeNow = date('H:i');
+
+if ($timeNow >= date('08:00') && $timeNow <= date('21:00')) { 
+    if (($dateNow - $lastRefreshDate)>= 1) {
+        getAPIdata();
+    }
+}
--- extraerDatosAPI.php
+++ extraerDatosAPI.php
@@ -1,33 +1,38 @@
 function getAPIdata()
 {
-$url = "https://LaAPIqueQuieras";
+    set_time_limit(600);
+    $url = "https://instagram29.p.rapidapi.com/user/ermuakirolak/media";
 
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-$headers = array(
-"NombredelHeader: contenido",
-"NombredelHeader: contenido",
-);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-//for debug only!
-//curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    $headers = array(
+        "X-RapidAPI-Host: instagram29.p.rapidapi.com",
+        "X-RapidAPI-Key: 83a70cd171msh533a87840ed50cep1d7ad8jsn12c6a48f1868",
+    );
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    //for debug only!
+    //curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    //curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-$resp = curl_exec($curl);
-if ($resp === false)
-$resp = curl_error($curl);
-//dump($resp);
-curl_close($curl);
+    $resp = curl_exec($curl);
 
-$decodedResp = json_decode($resp, true);
+    if ($resp === false){
+        $resp = curl_error($curl);
+    }else{
+        if(json_decode($resp, true)['data']){
+            //dump($resp);
 
-$publicacionTexto = $decodedResp['data']['media'][0]['caption'];
-$fechaPub = $decodedResp['data']['media'][0]['taken_at'];
-$link = $decodedResp['data']['media'][0]['shortcode'];
+            $decodedResp = json_decode($resp, true);
 
-$sql = "UPDATE social_media SET textoPub = '". $publicacionTexto ."', fechaPub = '". $fechaPub . "', link ='". $link . "', fechaMod = '". date("Y/m/d h:i:s") ."' WHERE id = 1";
-mysql_query($sql);
+            $publicacionTexto = $decodedResp['data']['media'][0]['caption'];
+            $fechaPub = $decodedResp['data']['media'][0]['taken_at'];
+            $link = $decodedResp['data']['media'][0]['shortcode'];
+
+            $sql = "UPDATE social_media SET textoPub = '" . $publicacionTexto . "', fechaPub = '" . $fechaPub . "', link ='" . $link . "', fechaMod = '" . date("Y/m/d h:i:s") . "' WHERE id = 1";
+            mysql_query($sql);
+        }
+    }
 
 }
